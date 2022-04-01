990bbd7778512cc0545e4f480518f1539e47f9ce
zrcni <zrcni9@gmail.com>
2022-04-01 09:36:02 +0300

    added screenshot to readme

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Kibana is available at http://localhost:5601
 Run webapp once to generate logs to elasticsearch
 
 then go to `Settings > Index Patterns > Create index pattern` in Kibana and create an index pattern for `webapp-service-logs`
+
+---
+
+## Search logs by metadata fields in Kibana
+
+![kibana](./kibana.png)
--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,6 +1,7 @@
 const logger = require("./logger")
 
 logger.info("service started")
+
 logger.info("user logged in {userId}", { userId: "12345" })
 logger.info("user logged in {userId}", { userId: "56789" })
 logger.info("user logged in {userId}", { userId: "34567" })
@@ -8,4 +9,5 @@ logger.info("user logged in {userId}", { userId: "34567" })
 logger.info("user logged out {userId}", { userId: "12345" })
 logger.info("user logged out {userId}", { userId: "56789" })
 logger.info("user logged out {userId}", { userId: "34567" })
+
 logger.info("service stopped")
